8cba921bdff4682601be437066b56f38d2e20d83
NaNaNaS <93786072+NaNaNaS526@users.noreply.github.com>
2023-02-10 20:51:42 +0500

    Select and interact counters

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,13 +1,22 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
     private PlayerInputActions _playerInputActions;
+    public event EventHandler OnInteractAction;
 
     private void Awake()
     {
         _playerInputActions = new PlayerInputActions();
         _playerInputActions.Player.Enable();
+        _playerInputActions.Player.Interact.performed += InteractPerformed;
+    }
+
+    private void InteractPerformed(InputAction.CallbackContext ctx)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
     }
 
     public Vector2 GetMovementVectorNormalized()
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,96 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { get; private set; }
+
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+
+    public class OnSelectedCounterChangedEventArgs : EventArgs
+    {
+        public ClearCounter SelectedCounter;
+    }
+
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
     [SerializeField] private float moveSpeed = 6.0f;
     [SerializeField] private float rotateSpeed = 10.0f;
     private bool _isWalking;
+    private Vector3 _lastInteractDirection;
+    private ClearCounter _selectedCounter;
+
+    private void Awake()
+    {
+        if (!Instance) Instance = this;
+    }
+
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInputOnInteractAction;
+    }
+
 
     private void Update()
     {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking()
+    {
+        return _isWalking;
+    }
+
+    private void GameInputOnInteractAction(object sender, EventArgs e)
+    {
+        if (_selectedCounter != null)
+        {
+            _selectedCounter.Interact();
+        }
+    }
+
+    private void HandleInteractions()
+    {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDirection = new Vector3(inputVector.x, 0.0f, inputVector.y).normalized;
+        if (moveDirection != Vector3.zero) _lastInteractDirection = moveDirection;
+
+        float interactDistance = 2.0f;
+        if (Physics.Raycast(transform.position, _lastInteractDirection, out RaycastHit raycastHit, interactDistance,
+                countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                if (clearCounter != _selectedCounter)
+                {
+                    SetSelectedCounter(clearCounter);
+                }
+            }
+            else
+            {
+                SetSelectedCounter(null);
+            }
+
+            Debug.Log(_selectedCounter);
+        }
+        else
+        {
+            SetSelectedCounter(null);
+        }
+    }
+
+    private void HandleMovement()
+    {
+        var playerTransform = transform;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDirection = new Vector3(inputVector.x, 0.0f, inputVector.y).normalized;
+        transform.forward = Vector3.Slerp(playerTransform.forward, moveDirection, Time.deltaTime * rotateSpeed);
 
         float moveDistance = moveSpeed * Time.deltaTime;
         float playerRadius = 0.55f;
         float playerHeight = 2.0f;
 
-        var playerTransform = transform;
         var playerPosition = playerTransform.position;
         bool canMove = !Physics.CapsuleCast(playerPosition, playerPosition + Vector3.up * playerHeight,
             playerRadius, moveDirection, moveDistance);
@@ -47,12 +121,12 @@ public class Player : MonoBehaviour
         }
 
         _isWalking = moveDirection != Vector3.zero;
-
-        transform.forward = Vector3.Slerp(playerTransform.forward, moveDirection, Time.deltaTime * rotateSpeed);
     }
 
-    public bool IsWalking()
+    private void SetSelectedCounter(ClearCounter selectedCounter)
     {
-        return _isWalking;
+        this._selectedCounter = selectedCounter;
+        OnSelectedCounterChanged?.Invoke(this,
+            new OnSelectedCounterChangedEventArgs { SelectedCounter = _selectedCounter });
     }
 }
